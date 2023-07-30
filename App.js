5de0717e55fb1572e87b6c906079f163f9e72e55
KarenChobanyan <karen.ysu7@mail.ru>
2023-07-30 12:14:08 +0400

    animations are complieted

--- App.js
+++ App.js
@@ -1,31 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { useRef } from 'react';
-import { Animated, StyleSheet, Text, View,Button, ScrollView } from 'react-native';
+import { useMemo } from 'react';
+import { StyleSheet, View, FlatList, } from 'react-native';
 import BounceAnimation from './assets/components/BounceAnimation';
 import BallAnimation from './assets/components/BallAnimation';
 import HeartAnimation from './assets/components/HeartAnimation';
 import BoxAnimation from './assets/components/BoxAnimation';
 import RainyAnimation from './assets/components/RainyAnimation';
+import AnimatedItem from './assets/components/AnimatedItem';
 
 export default function App() {
-  return (
-    <ScrollView>
-    <View style={styles.container}>
-     <BounceAnimation/>
-     <BallAnimation/>
-     <HeartAnimation/>
-     <BoxAnimation/>
-     <RainyAnimation/>
-    </View>
-    </ScrollView>
-  );
-}
+  const animations = useMemo(() => {
+    return [
+      {
+        item: <BounceAnimation />,
+        id: "bounce"
+      },
+      {
+        item: <BallAnimation />,
+        id: "ball"
+      },
+      {
+        item: <HeartAnimation />,
+        id: "heart",
+      },
+      {
+        item: <BoxAnimation />,
+        id: "box"
+      },
+      {
+        item: <RainyAnimation />,
+        id: "rain"
+      }
+    ]
 
+  }, [])
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={animations}
+        renderItem={({ item }) => <AnimatedItem children={item.item} />}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#35168A',
     alignItems: 'center',
-    padding:5
+    padding: 5
   },
 });
